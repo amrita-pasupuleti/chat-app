46175f0a8a30241f3603a8932969122cee515806
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2024-08-22 17:41:57 -0700

    frontend update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/list/chatList/addUser.jsx
@@ -0,0 +1,5 @@
+const addUser = () => {
+  return <div>addUser</div>;
+};
+
+export default addUser;
