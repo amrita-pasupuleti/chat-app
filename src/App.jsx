02fc3b55ea9faed3e1dfdd49cb14e0112b2ade46
Amrita Pasupuleti <amritapasupuleti@gmail.com>
2024-08-24 23:48:35 -0700

    update firebase login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
-  const user = false;
+  // const user = false;
+
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user.uid);
+      fetchUserInfo(user.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if (isLoading) return <div className="loading">Loading . . .</div>;
 
   return (
     <div className="container">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
